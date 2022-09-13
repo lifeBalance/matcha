774fb102290c78a6346f31bee501f55d497bf75c
javi <3051496+lifeBalance@users.noreply.github.com>
2022-09-14 00:26:57 +0300

    install front-end dependencies: react-redux, rtk, react-router, tailwind

--- www/dev/src/App.jsx
+++ www/dev/src/App.jsx
@@ -1,9 +1,14 @@
-import { useState } from 'react'
+import React from 'react'
+import Peach from './assets/peach.png'
+import {EnvelopeIcon} from './assets/icons'
 
 function App() {
 
   return (
-    <h1>hi yall</h1>
+    <>
+      <h1 className='text-6xl font-bold font-logo'>m<img src={Peach} alt="" className='inline w-9' />tch<img src={Peach} alt="" className='inline w-9' /></h1>
+      <EnvelopeIcon />
+    </>
   )
 }
 
---
+++ www/dev/tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      logo: ['All Round Gothic', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
