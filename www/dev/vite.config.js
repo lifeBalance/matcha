52cdc52306ee2d57e441c8d0ec53ac72fc94b782
javi <3051496+lifeBalance@users.noreply.github.com>
2022-09-16 17:25:14 +0300

    fix serving API over HTTPS when developing (install basicSSl plugin for Vite, and adjust vite.config.js)

--- www/dev/vite.config.js
+++ www/dev/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,10 +8,14 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api/': {
-        target: 'http://127.0.0.1:80',
-        changeOrigin: true,
+        // target: 'http://127.0.0.1:80',
+        target: 'https://127.0.0.1',
+        secure: false
       }
     }
   },
-  plugins: [react()]
+  plugins: [
+    react(),
+    basicSsl()
+  ]
 })
