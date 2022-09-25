c42979d3e8f42c8662a51957ffb006ddf81ba6c4
javi <3051496+lifeBalance@users.noreply.github.com>
2022-09-25 12:21:48 +0300

    add maximum width to contents of navbar and footer so they dont go to the extremes of the page

--- www/dev/src/components/UI/Navbar/Navbar.jsx
+++ www/dev/src/components/UI/Navbar/Navbar.jsx
@@ -17,32 +17,34 @@ function Navbar() {
   const isLoggedIn = useSelector(slices => slices.auth.isLoggedIn)
 
   return (
-    <nav className='flex justify-between items-center bg-black py-3 px-8'>
-      <Logo styles='text-6xl font-bold font-logo text-white min-w-fit' />
+    <nav className='bg-black'>
+      <div className="max-w-6xl flex justify-between items-center mx-auto py-3 xl:px-0 md:px-8 px-4">
+        <Logo styles='text-6xl font-bold font-logo text-white min-w-fit' />
 
-      <div className='hidden md:flex md:space-x-8 text-white'>
-        <MenuItem title='test' route='test' />
-        {isLoggedIn ? 
-          (<>
-            <MenuItem title='users' route='users' />
-            <MenuItem title='profile' route='profile' />
+        <div className='hidden md:flex md:space-x-8 text-white'>
+          <MenuItem title='test' route='test' />
+          {isLoggedIn ? 
+            (<>
+              <MenuItem title='users' route='users' />
+              <MenuItem title='profile' route='profile' />
 
-            <div className='group'>
-              <p onClick={() => dispatch(logout())} className='text-red-500 hover:cursor-pointer'>log out</p>
+              <div className='group'>
+                <p onClick={() => dispatch(logout())} className='text-red-500 hover:cursor-pointer'>log out</p>
 
-              <div className='mx-2 group-hover:border-b group-hover:border-red-500'></div>
-            </div>
-          </>)
-          :
-          (<>
-            <MenuItem title='login' route='login' />
-          </>)
-        }
+                <div className='mx-2 group-hover:border-b group-hover:border-red-500'></div>
+              </div>
+            </>)
+            :
+            (<>
+              <MenuItem title='login' route='login' />
+            </>)
+          }
+        </div>
+
+        <Burger />
+
+        {isOpen && <MobileMenu />}
       </div>
-
-      <Burger />
-
-      {isOpen && <MobileMenu />}
     </nav>
   )
 }
