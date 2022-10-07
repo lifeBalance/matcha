a23db02d99355713b97ad6211a4c6ba7b480c227
javi <3051496+lifeBalance@users.noreply.github.com>
2022-10-07 19:57:59 +0300

    modify DB: remove profiles table; add it all to users table

--- app/docker/mysql/db_setup.sql
+++ app/docker/mysql/db_setup.sql
@@ -9,8 +9,23 @@ CREATE TABLE IF NOT EXISTS `users` (
     `lastname` VARCHAR(50) NOT NULL,
     `email` VARCHAR(255) UNIQUE NOT NULL,
     `pwd_hash` VARCHAR(255) NOT NULL,
-    `confirmed` BOOLEAN NOT NULL DEFAULT 0
+    `confirmed` BOOLEAN NOT NULL DEFAULT 0,
+    `age` INT UNSIGNED DEFAULT NULL,
+    `gender` INT UNSIGNED DEFAULT 2,
+    `prefers` INT UNSIGNED DEFAULT 2,
+    `bio` VARCHAR(255) DEFAULT '',
+    `profile_pic` VARCHAR(255) DEFAULT ''
 );
+
+-- CREATE TABLE IF NOT EXISTS `users` (
+--     `id` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+--     `username` VARCHAR(50) NOT NULL,
+--     `firstname` VARCHAR(50) NOT NULL,
+--     `lastname` VARCHAR(50) NOT NULL,
+--     `email` VARCHAR(255) UNIQUE NOT NULL,
+--     `pwd_hash` VARCHAR(255) NOT NULL,
+--     `confirmed` BOOLEAN NOT NULL DEFAULT 0
+-- );
 
 CREATE TABLE IF NOT EXISTS `refresh_tokens` (
     `user_id` INT UNSIGNED,
@@ -24,19 +39,20 @@ CREATE TABLE IF NOT EXISTS `email_tokens` (
     `expires_at` INT UNSIGNED NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `profiles` (
-    `id` INT UNSIGNED PRIMARY KEY NOT NULL,
-    `age` INT UNSIGNED DEFAULT 18,
-    `gender` INT UNSIGNED DEFAULT 2,
-    `prefers` INT UNSIGNED DEFAULT 2,
-    `bio` VARCHAR(255) DEFAULT '',
-    `profile_pic` VARCHAR(255) DEFAULT ''
-);
+-- CREATE TABLE IF NOT EXISTS `profiles` (
+--     `id` INT UNSIGNED PRIMARY KEY NOT NULL,
+--     `age` INT UNSIGNED DEFAULT 18,
+--     `gender` INT UNSIGNED DEFAULT 2,
+--     `prefers` INT UNSIGNED DEFAULT 2,
+--     `bio` VARCHAR(255) DEFAULT '',
+--     `profile_pic` VARCHAR(255) DEFAULT ''
+-- );
 
 CREATE TABLE IF NOT EXISTS `checked_users` (
     `id` INT UNSIGNED PRIMARY KEY NOT NULL,
     `checked_by` INT UNSIGNED NOT NULL
 );
+
 CREATE TABLE IF NOT EXISTS `liked_users` (
     `id` INT UNSIGNED PRIMARY KEY NOT NULL,
     `likes` INT UNSIGNED NOT NULL
--- app/src/db/seeds/addUsers.js
+++ app/src/db/seeds/addUsers.js
@@ -21,67 +21,19 @@ for (let index = 1; index <= 500; index++) {
     lastname: faker.name.lastName(),
     email: faker.internet.email(),
     pwd_hash: '$2y$10$m2E7H1mg/MJ7.4cwqIDiTe/iJ48i/yULkd/XLVSRL4RUE8qf5jkcG',
-    confirmed: 1
-  })
-}
-
-// generate an in-memory array of profiles
-const profiles = []
-users.forEach(user => {
-  profiles.push({
-    id: user.id,
+    confirmed: 1,
     age: getRandomInt(18, 99),
     gender: getRandomInt(0, 3),   // either 0, 1, or 2.
     prefers: getRandomInt(0, 3),  // either 0, 1, or 2.
     bio: faker.hacker.phrase(),
     profile_pic: faker.image.people(480, 480)
   })
-})
+}
 
 exports.seed = async function(knex) {
   // Deletes existing entries in ALL tables
   await knex('users').del()
-  await knex('profiles').del()
 
   // Write new ones
   await knex('users').insert(users)
-  // await knex('users').insert([
-  //   {
-  //     id: 1,
-  //     username: 'crazyBear',
-  //     firstname: 'Alotto',
-  //     lastname: 'Dicks',
-  //     email: 'test@test.com',
-  //     pwd_hash: '$2y$10$m2E7H1mg/MJ7.4cwqIDiTe/iJ48i/yULkd/XLVSRL4RUE8qf5jkcG',
-  //     confirmed: 1
-  //   },
-  //   {
-  //     id: 2,
-  //     username: 'bjQueen',
-  //     firstname: 'Thea',
-  //     lastname: 'Baggins',
-  //     email: 'test@test2.com',
-  //     pwd_hash: '$2y$10$m2E7H1mg/MJ7.4cwqIDiTe/iJ48i/yULkd/XLVSRL4RUE8qf5jkcG',
-  //     confirmed: 1
-  //   },
-  // ])
-  await knex('profiles').insert(profiles)
-  // await knex('profiles').insert([
-  //   {
-  //     id: 1,
-  //     age: 32,
-  //     gender: 1,
-  //     prefers: 0,
-  //     bio: "I like long walks in the beach with my girlfriend, until the acid wears off, and I realize I'm draggin a manequin through the parking lot.",
-  //     profile_pic: ''
-  //   },
-  //   {
-  //     id: 2,
-  //     age: 35,
-  //     gender: 0,
-  //     prefers: 2,
-  //     bio: "If you are looking for a woman with personality, well it's your lucky day, because I have multiple.",
-  //     profile_pic: ''
-  //   },
-  // ])
 }
