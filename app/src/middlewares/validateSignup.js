d5deeebb2ce9dc16bf95ff6cbc15e00a9ad50754
javi <3051496+lifeBalance@users.noreply.github.com>
2022-10-08 21:32:02 +0300

    move signup form validation middleware to a separate file

---
+++ app/src/middlewares/validateSignup.js
@@ -0,0 +1,26 @@
+// Validators for all signup fields (same code as in React front-end)
+const {
+  validateUsername,
+  validatePassword,
+  validateEmail,
+  validateName
+} = require('../utils/validators')
+
+exports.validateSignup = (req, res, next) => {
+  if (!req.hasOwnProperty('body') ||
+    !req.body.hasOwnProperty('username') ||
+    !req.body.hasOwnProperty('firstname') ||
+    !req.body.hasOwnProperty('lastname') ||
+    !req.body.hasOwnProperty('email') ||
+    !req.body.hasOwnProperty('password') ||
+    !validateUsername(req.body.username) ||
+    !validateName(req.body.firstname) ||
+    !validateName(req.body.lastname) ||
+    !validateEmail(req.body.email) ||
+    !validatePassword(req.body.password))
+  {
+    res.status(400).json({ message: 'bad request' })
+    return
+  }
+  next()
+}
--- app/src/routes/accounts.js
+++ app/src/routes/accounts.js
@@ -1,41 +1,15 @@
 const express = require('express')
-// Validators for all signup fields (same code as in React front-end)
-const {
-  validateUsername,
-  validatePassword,
-  validateEmail,
-  validateName
-} = require('../utils/validators')
-
-const accountsController = require('../controllers/accounts')
-
 const router = express.Router()
 
-// Middleware to validate usernames
+// Naturally we need the 'accounts' controller
+const accountsController = require('../controllers/accounts')
+
+// Middleware for our routes
 const { validateUsernames } = require('../middlewares/validateUsernames')
+const { validateSignup } = require('../middlewares/validateSignup')
 
-// middleware to validate 'em all, son
-function mwValidateSignup(req, res, next) {
-  if (!req.hasOwnProperty('body') ||
-    !req.body.hasOwnProperty('username') ||
-    !req.body.hasOwnProperty('firstname') ||
-    !req.body.hasOwnProperty('lastname') ||
-    !req.body.hasOwnProperty('email') ||
-    !req.body.hasOwnProperty('password') ||
-    !validateUsername(req.body.username) ||
-    !validateName(req.body.firstname) ||
-    !validateName(req.body.lastname) ||
-    !validateEmail(req.body.email) ||
-    !validatePassword(req.body.password))
-  {
-    res.status(400).json({ message: 'bad request' })
-    return
-  }
-  next()
-}
-
-// POST => /api/accounts (for creating account)
-router.post('/accounts', mwValidateSignup, accountsController.create)
+// POST => /api/accounts (for creating accounts)
+router.post('/accounts', validateSignup, accountsController.create)
 
 // GET => /api/accounts (check if username exists; useful at signup)
 router.get('/accounts', validateUsernames, accountsController.readUsername)
