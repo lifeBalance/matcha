4147e6aa753cc4cf24fe1985e72b7deff5ca748f
javi <3051496+lifeBalance@users.noreply.github.com>
2022-11-17 14:30:43 +0200

    fix Map marker icon image not showing up

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -9,6 +9,17 @@ import {
 } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
+import L from 'leaflet'
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
 function Map(props) {
   function SetView({ center }) {
     const map = useMap()
